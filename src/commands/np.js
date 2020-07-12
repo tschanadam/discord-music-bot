e28bee0409d10c15017f27c9f25751493e474827
tschanadam <63157695+tschanadam@users.noreply.github.com>
2020-07-12 17:34:32 -0400

    Update np.js

--- src/commands/np.js
+++ src/commands/np.js
@@ -3,8 +3,18 @@ module.exports = {
 	description: 'Now playing command.',
 	cooldown: 5,
 	execute(message) {
+		const npsongplaying = new Discord.MessageEmbed()
+	.setColor('#7289da')
+	.setTitle('Now playing:')
+	.setDescription('**${serverQueue.songs[0].title}**')
+	.setTimestamp()
+		const npnosong
+	.setColor('#7289da')
+	.setTitle('Now playing:')
+	.setDescription('Nothing is playing! Run !play (song) to start!')
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('There is nothing playing.');
-		return message.channel.send(`🎶 Now playing: **${serverQueue.songs[0].title}**`);
+		if (!serverQueue) return channel.send(npnosong);
+		return message.channel.send(npsongplaying);
+	
 	}
 };
