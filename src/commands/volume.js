4108e6a2f648e76a3c3e5b5cfac29da3b09a9c8a
tschanadam <63157695+tschanadam@users.noreply.github.com>
2020-07-12 22:15:36 -0400

    Update volume.js

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -3,13 +3,29 @@ module.exports = {
 	description: 'Volume command.',
 	cooldown: 5,
 	execute(message, args) {
+		const errNoVC = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('An error was encountered:')
+	.setDescription('Please connect to a voice channel to execute this command!')
+		const noMusicPlaying = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('An error was encountered:')
+	.setDescription('There is no music playing.')
+		const currentVolume = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('The current volume is: **${serverQueue.volume}**')
+	.setDescription('To change it, please type !volume followed by a number between 1 and 5.')
+		const volumeSet = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Success!')
+	.setDescription('I changed the volume to: **${args[0]}**')
 		const { channel } = message.member.voice;
-		if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+		if (!channel) return message.channel.send(errNoVC);
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('There is nothing playing.');
-		if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
+		if (!serverQueue) return message.channel.send(noMusicPlaying);
+		if (!args[0]) return message.channel.send(currentVolume);
 		serverQueue.volume = args[0]; // eslint-disable-line
 		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-		return message.channel.send(`I set the volume to: **${args[0]}**`);
+		return message.channel.send(volumeSet);
 	}
 };
