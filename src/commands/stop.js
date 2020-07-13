ae633e11093045564111bfc5cb8351e6da592bcc
tschanadam <63157695+tschanadam@users.noreply.github.com>
2020-07-12 22:10:15 -0400

    Update stop.js

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -3,11 +3,23 @@ module.exports = {
 	description: 'Stop command.',
 	cooldown: 5,
 	execute(message) {
+		const errNoVC = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('An error was encountered:')
+	.setDescription('Please connect to a voice channel to execute this command!')
+		const noEligibleSongs = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('An error was encountered:')
+	.setDescription('There is nothing playing I can stop for you.')
+		const songStopped = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Success!')
+	.setDescription('I stopped the song for you.')
 		const { channel } = message.member.voice;
-		if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+		if (!channel) return message.channel.send(errNoVC);
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('There is nothing playing that I could stop for you.');
+		if (!serverQueue) return message.channel.send(noEligibleSongs);
 		serverQueue.songs = [];
-		serverQueue.connection.dispatcher.end('Stop command has been used!');
+		serverQueue.connection.dispatcher.end(songStopped);
 	}
 };
