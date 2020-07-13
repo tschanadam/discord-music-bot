004629bebffa37715af3a3fb325395db0a92f3a5
tschanadam <63157695+tschanadam@users.noreply.github.com>
2020-07-12 21:48:35 -0400

    Update queue.js

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -3,6 +3,11 @@ module.exports = {
 	description: 'Queue command.',
 	cooldown: 5,
 	execute(message) {
+		const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('**__MUSIC QUEUE__**')
+	.setDescription('${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}')
+	.addField(name: '**__NOW PLAYING**__', value: '${serverQueue.songs[0].title}')
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send('There is nothing playing.');
 		return message.channel.send(`
