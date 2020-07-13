fa49e99d7b0e202370900a552898133d04eb7293
tschanadam <63157695+tschanadam@users.noreply.github.com>
2020-07-12 21:54:55 -0400

    Update queue.js

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -3,19 +3,16 @@ module.exports = {
 	description: 'Queue command.',
 	cooldown: 5,
 	execute(message) {
-		const exampleEmbed = new Discord.MessageEmbed()
+		const queue = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('**__MUSIC QUEUE__**')
 	.setDescription('${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}')
 	.addField(name: '**__NOW PLAYING**__', value: '${serverQueue.songs[0].title}')
+		const noSongs = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('An error was encountered:')
+	.setDescription('There currently is nothing playing. Try running !play (song)')
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('There is nothing playing.');
-		return message.channel.send(`
-__**Song queue:**__
-
-${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
-
-**Now playing:** ${serverQueue.songs[0].title}
-		`);
-	}
+		if (!serverQueue) return message.channel.send('noSongs');
+		return message.channel.send(queue)
 };
