28d82a25da2b2bd412b900f32c648cc104666cf0
tschanadam <63157695+tschanadam@users.noreply.github.com>
2020-07-12 22:00:36 -0400

    Update resume.js

--- src/commands/resume.js
+++ src/commands/resume.js
@@ -3,12 +3,20 @@ module.exports = {
 	description: 'Resume command.',
 	cooldown: 5,
 	execute(message) {
+		const noSongs = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('An error was encountered:')
+	.setDescription('There currently is nothing playing. Try running !play (song)')
+		const resumeSuccess = new Discord.messageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Success!')
+	.setDescription('I resumed the music!')
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (serverQueue && !serverQueue.playing) {
 			serverQueue.playing = true;
 			serverQueue.connection.dispatcher.resume();
-			return message.channel.send('▶ Resumed the music for you!');
+			return message.channel.send(resumeSuccess);
 		}
-		return message.channel.send('There is nothing playing.');
+		return message.channel.send(noSongs);
 	}
 };
